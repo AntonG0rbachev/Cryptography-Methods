b385f89114c45563428214f4c558655d963b8985
DocLivsey <doclivsey@github.com>
2024-12-14 13:25:44 +0300

    feat: change algorithm

--- Ferma/ferma.py
+++ Ferma/ferma.py
@@ -24,18 +24,15 @@ def ferma(n):
         б) Если нет, увеличиваем x на 1 и повторяем шаги.
     4. Останавливаемся, когда множители найдены.
     """
-    if n % 2 == 0:
-        return 2, n // 2
-
-    x = math.isqrt(n) + 1
-    y = x * x - n
-
-    while not is_square(y):
+    x = math.ceil(math.sqrt(n))
+    while True:
+        y_square = x * x - n
+        y = int(math.sqrt(y_square))
+        if y * y == y_square:
+            p1 = x - y
+            p2 = x + y
+            return p1, p2
         x += 1
-        y = x * x - n
-
-    y = int(math.sqrt(y))
-    return x - y, x + y
 
 
 if __name__ == '__main__':
