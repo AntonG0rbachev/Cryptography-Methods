c8f61918308f49ac5752a34f07fd0d11fc41a8d8
DocLivsey <doclivsey@github.com>
2024-12-21 03:07:20 +0300

    feat: added case with mod == none

--- QuickPow/quick_pow.py
+++ QuickPow/quick_pow.py
@@ -21,8 +21,8 @@ def quick_pow(base, exp, mod=None):
 
     while exp > 0:
         if exp % 2 == 1:
-            result = (result * base) % mod
-        base = (base * base) % mod
+            result = (result * base) % mod if mod else (result * base)
+        base = (base * base) % mod if mod else (base * base)
         exp //= 2
 
     return result
