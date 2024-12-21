57d9c1e56240af1309299a8baa21629e67606c62
DocLivsey <doclivsey@github.com>
2024-12-21 04:28:26 +0300

    feat: added calls script as main with arguments (params)

--- DiscreteLogarithm/baby-step_giant-step_log.py
+++ DiscreteLogarithm/baby-step_giant-step_log.py
@@ -3,6 +3,8 @@
 g^x == h (mod p)
 Цель - найти такое х, что g^x mod p == h
 """
+import sys
+
 from QuickPow.quick_pow import quick_pow
 
 
@@ -38,10 +40,33 @@ def baby_step_giant_step(n, p, g, h):
 
 
 if __name__ == '__main__':
-    p = 251
-    n = 250
-    g = 11
-    h = 58
+    args = sys.argv
+
+    defaults = {
+        'p': 251,
+        'n': 250,
+        'g': 11,
+        'h': 58,
+    }
+    p, n, g, h = None, None, None, None
+
+    if len(args) <= 1:
+        p = defaults['p']
+        n = defaults['n']
+        g = defaults['g']
+        h = defaults['h']
+
+    else:
+        args = args[1::]
+        args_map = dict()
+        if len(args) % 2 != 0:
+            raise Exception("there's not enough argument")
+        for i in range(0, len(args) - 1, 2):
+            args_map[args[i]] = args[i + 1]
+        p = float(args_map['-p']) if '-p' in args_map.keys() else defaults['p']
+        n = float(args_map['-n']) if '-n' in args_map.keys() else p - 1 if '-p' in args_map.keys() else defaults['n']
+        g = float(args_map['-g']) if '-g' in args_map.keys() else defaults['g']
+        h = float(args_map['-h']) if '-h' in args_map.keys() else defaults['h']
 
     print(baby_step_giant_step(n, p, g, h))
 
